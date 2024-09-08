c85ba008d4cfb8a4f2da5f03e991f2ea51c9bb9f
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-09-08 11:53:11 +0200

    fix shell info

--- internal/flags.go
+++ internal/flags.go
@@ -8,8 +8,8 @@ func DefineFlags() (helpFlag, dryRunFlag, allIdesFlag, allFilesFlag, repatchFlag
 	helpFlag = pflag.BoolP("help", "h", false, "Show help")
 	dryRunFlag = pflag.BoolP("dry-run", "d", false, "Dry run")
 	allIdesFlag = pflag.BoolP("all-ides", "a", false, "Select all IDEs")
-	allFilesFlag = pflag.BoolP("all-files", "y", false, "Select all IDEs")
-	repatchFlag = pflag.BoolP("repatch", "r", false, "Select all IDEs")
+	allFilesFlag = pflag.BoolP("all-files", "y", false, "Select all files")
+	repatchFlag = pflag.BoolP("repatch", "r", false, "Repatch")
 	currentShellFlag = pflag.BoolP("current-shell", "c", false, "Use current shell from $SHELL")
 	return
 }
